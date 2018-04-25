6b77e36df8404e88847b613c257f9f611c8e105c
asabagh <a.sabagh72@gmail.com>
2018-04-26 00:17:40 +0430

    uninstall plugin

---
+++ uninstall.php
@@ -0,0 +1,18 @@
+<?php
+// if uninstall.php is not called by WordPress, die
+if (!defined('WP_UNINSTALL_PLUGIN')) {
+    die;
+}
+//delte options
+$options = array(
+    "lj_setting_option",
+    "lj_configration_dissmiss"
+);
+foreach ($options as $option) {
+    if (get_option($option)) {
+        delete_option($option);
+    }
+}
+// drop a metadata
+global $wpdb;
+$wpdb->query("DELETE * FROM {$wpdb->prefix}postmeta WHERE meta_key = 'lj_like_wp'");
