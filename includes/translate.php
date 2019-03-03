dad038140a9097e163895d7c6b7e476e5c1a8702
asabagh <a.sabagh72@gmail.com>
2019-03-03 15:29:38 +0330

    translate plugin headers

---
+++ includes/translate.php
@@ -0,0 +1,4 @@
+<?php
+__('RNG_AjaxLike','rng-ajaxlike');
+__('WordPress plugin that allow the visitor to like posts content in a single page using Ajax technology with excellent features like setting panel and widget that show posts order by like counts.','rng-ajaxlike');
+__('Abolfazl Sabagh','rng-ajaxlike');
