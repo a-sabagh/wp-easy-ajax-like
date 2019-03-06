f4c8abacf3744ad5c6b7bcad29d34a3517e1c6f6
asabagh <a.sabagh72@gmail.com>
2019-03-06 14:39:36 +0330

    complate annotation

--- includes/class.init.php
+++ includes/class.init.php
@@ -1,48 +1,68 @@
 <?php
 
-class rajl_init{
+class rajl_init {
+
+    /**
+     * plugin version
+     * @var Integer
+     */
     public $version;
+
+    /**
+     * plugin slug used for text domain
+     * @var Integer
+     */
     public $slug;
-    
-    public function __construct($version,$slug){
+
+    /**
+     * constructor
+     * @param Integer $version
+     * @param Integer $slug
+     */
+    public function __construct($version, $slug) {
         $this->version = $version;
         $this->slug = $slug;
         add_action('plugins_loaded', array($this, 'add_text_domain'));
-        add_action('admin_enqueue_scripts',array($this,'admin_enqueue_scripts'));
-        add_action('wp_enqueue_scripts',array($this,'public_enqueue_scripts'));
+        add_action('admin_enqueue_scripts', array($this, 'admin_enqueue_scripts'));
+        add_action('wp_enqueue_scripts', array($this, 'public_enqueue_scripts'));
         $this->load_modules();
     }
+
     /**
      * add text domain with rng-ajaxlike
      * call translate string file
      * configure multilanguage plugin
      */
-    public function add_text_domain(){
+    public function add_text_domain() {
         load_plugin_textdomain($this->slug, FALSE, RAJL_PRT . "/languages");
         include trailingslashit(__DIR__) . "translate.php";
     }
+
     /**
      * enqueue admin scripts and style
      * @param Array $hooks
      */
-    public function admin_enqueue_scripts($hooks){
-        wp_enqueue_style( 'wp-color-picker' ); 
-        wp_enqueue_style("lj-admin-style",RAJL_PDU . "/assets/css/admin-style.css");
-        wp_enqueue_script("lj-admin-script",RAJL_PDU . "assets/js/admin-script.js",array("jquery","wp-color-picker"), "" , TRUE);
+    public function admin_enqueue_scripts($hooks) {
+        wp_enqueue_style('wp-color-picker');
+        wp_enqueue_style("lj-admin-style", RAJL_PDU . "/assets/css/admin-style.css");
+        wp_enqueue_script("lj-admin-script", RAJL_PDU . "assets/js/admin-script.js", array("jquery", "wp-color-picker"), "", TRUE);
     }
+
     /**
      * enqueue public script and style
      */
-    public function public_enqueue_scripts(){
-        wp_enqueue_style("lj-style",RAJL_PDU . "/assets/css/style.css");
-        wp_enqueue_script("lj-script",RAJL_PDU . "/assets/js/script.js",array("jquery"), "" , TRUE);
+    public function public_enqueue_scripts() {
+        wp_enqueue_style("lj-style", RAJL_PDU . "/assets/css/style.css");
+        wp_enqueue_script("lj-script", RAJL_PDU . "/assets/js/script.js", array("jquery"), "", TRUE);
     }
+
     /**
      * load plugin class file as modules
      */
-    private function load_modules(){
+    private function load_modules() {
         include trailingslashit(__DIR__) . "class.controller.settings.php";
         include trailingslashit(__DIR__) . "class.controller.like.php";
         include trailingslashit(__DIR__) . "widgets/mostliked-posts.php";
     }
+
 }
