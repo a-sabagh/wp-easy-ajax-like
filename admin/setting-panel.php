eb091414f4a28158769e4c001921fdf2d80d2f5a
asabagh <a.sabagh72@gmail.com>
2019-03-07 13:21:32 +0330

    update language 1551952275

--- admin/setting-panel.php
+++ admin/setting-panel.php
@@ -1,20 +1,24 @@
 <?php
-if(!current_user_can("manage_options")){
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
+}
+
+if (!current_user_can("manage_options")) {
     return;
 }
-if(isset($_GET['settings-updated']) and $_GET['settings-updated'] == TRUE){
-    add_settings_error("rajl_general_setting", "rajl_general_setting" , __("Settings saved","rng-ajaxlike") , "updated");
-}elseif(isset($_GET['settings-updated']) and $_GET['settings-updated'] == FALSE){
-    add_settings_error("rajl_general_setting", "rajl_general_setting" , __("Error with saving","rng-ajaxlike"));
+if (isset($_GET['settings-updated']) and $_GET['settings-updated'] == TRUE) {
+    add_settings_error("rajl_general_setting", "rajl_general_setting", __("Settings saved", "rng-ajaxlike"), "updated");
+} elseif (isset($_GET['settings-updated']) and $_GET['settings-updated'] == FALSE) {
+    add_settings_error("rajl_general_setting", "rajl_general_setting", __("Error with saving", "rng-ajaxlike"));
 }
 ?>
 <div class="wrap">
     <h1><?php echo get_admin_page_title(); ?></h1>
     <form action="options.php" method="post">
-        <?php 
+        <?php
         settings_fields("rajl_general_settings");
         do_settings_sections("rajl_general_settings");
-        submit_button(__("save","rng-ajaxlike"));
+        submit_button(__("save", "rng-ajaxlike"));
         ?>
     </form>
 </div>
